58de1d1e74464c5942f81798153e1fb1f89ced89
Roland Samaha <rolandsamaha@gmail.com>
2022-12-09 17:09:03 -0800

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry poiny
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num < 10; num++)
-		putchar(num + '0');
+		putchar((num % 10) + '0');
+
 	putchar('\n');
+
 	return (0);
 }
