1b597f7f5570e0c2f86c5e40cfd96cc66a4cc7a3
Roland Samaha <rolandsamaha@gmail.com>
2022-12-13 15:43:13 -0800

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print alphabet_x10 - prints 10 times the alphabet in lowercase,
- * */
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase,
+ * followed by a new line
+ */
 
 void print_alphabet_x10(void)
 {
