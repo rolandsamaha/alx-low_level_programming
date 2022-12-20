54f75a7aff82c5b940f40b0cdee43954a82e9da5
Roland Samaha <rolandsamaha@gmail.com>
2022-12-19 17:10:09 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 		{
-			sign *= -1;
+			sign * = -1;
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
@@ -26,5 +26,5 @@ int _atoi(char *s)
 		}
 	} while (*s++);
 
-	return (num *sign);
+	return (num * sign);
 }
